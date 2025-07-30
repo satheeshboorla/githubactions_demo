d9c83ef5b878b4efe99be6da8d2e2c27a672185a
satheeshboorla <satheeshboorla143@gmail.com>
2025-07-31 00:12:13 +0530

    Update GetAPI.py

--- GetAPI.py
+++ GetAPI.py
@@ -8,3 +8,9 @@ headers = {}
 response = requests.request("GET", url, headers=headers, data=payload)
 
 print(response.text)
+
+with open("response.json", "w") as file:
+    json.dump(json_response, file, indent=4)
+
+print("JSON saved to 'data/response.json'")
+
